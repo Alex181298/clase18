b389a6981c53e2a7fe8b286871844d8c4885e486
Alejandro Manuel Velázquez García <alejandrogt18@live.com.mx>
2022-01-31 17:23:57 -0600

    usando templates

--- clase18/AppCoder/views.py
+++ clase18/AppCoder/views.py
@@ -10,3 +10,18 @@ def crear_curso(request,camada):
     curso.save()
     
     return HttpResponse(f'Usted ha creado un curso! {camada}')
+
+def inicio(request):
+    return render(request, 'AppCoder/inicio.html')
+
+def cursos(request):
+    return render(request, 'AppCoder/cursos.html')
+
+def profesores(request):
+    return HttpResponse('Vista Profesores')
+
+def estudiantes(request):
+    return HttpResponse('Vista Estudiantes')
+
+def entregables(request):
+    return HttpResponse('Vista Entregables')
